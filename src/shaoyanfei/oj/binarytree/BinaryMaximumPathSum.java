49e86954cd750755542dd617fd2e14d199745736
Yanfei Shao <yanfeishao@server.jackiedreamy.com>
2015-02-02 22:22:02 -0500

    Fix Bug
    fix bug

--- src/shaoyanfei/oj/binarytree/BinaryMaximumPathSum.java
+++ src/shaoyanfei/oj/binarytree/BinaryMaximumPathSum.java
@@ -2,13 +2,34 @@ package shaoyanfei.oj.binarytree;
 
 public class BinaryMaximumPathSum {
 
-	public int maxPathSum(TreeNode root) {
+	static int maxValue;
 
+	static public int maxPathSum(TreeNode root) {
+		maxValue = Integer.MIN_VALUE;
+		maxPathDown(root);
+		return maxValue;
+	}
+
+	static private int maxPathDown(TreeNode node) {
+		if (node == null)
+			return 0;
+		int left = Math.max(0, maxPathDown(node.left));
+		int right = Math.max(0, maxPathDown(node.right));
+		maxValue = Math.max(maxValue, left + right + node.val);
+		return Math.max(left, right) + node.val;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TreeNode treeNode = new TreeNode(1);
+		treeNode.left = new TreeNode(2);
+		// treeNode.left.left = new TreeNode(3);
+		treeNode.left.left = new TreeNode(3);
+		// treeNode.right = new TreeNode(3);
+		// treeNode.left.left.left = new TreeNode(4);
+		// treeNode.right.right = new TreeNode(6);
+		// treeNode.left.left.left.left = new TreeNode(5);
+		System.out.println(maxPathSum(treeNode));
 	}
 
 }
